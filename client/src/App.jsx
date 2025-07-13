b552f821970dbe312554e02fff7214685dd1ef6f
Abdul Razzaq <arrazzaq7860@gmail.com>
2025-07-13 20:20:10 +0530

    feat/addedNavbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from './api/axios';
+import './App.css'
+import  Navbar  from './components/common/Navbar.jsx';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -7,16 +9,19 @@ function App() {
   useEffect(() => {
     axios.get('/')
       .then(res => {setMessage(res.data.message)
-        console.log("hello")
       })
       .catch(err => console.error(err));
   }, []);
 
   return (
+      <>
+      <nav className="fixed top-0 left-0 w-full z-50 bg-white border-b border-gray-200 dark:bg-gray-900 shadow">
+        <Navbar/>
+      </nav>
+
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-900">
-      <h1 className="text-3xl font-bold mb-4">FitZone Admin Dashboard</h1>
-      <p className="text-xl">{message}</p>
     </div>
+      </>
   );
 }
 
