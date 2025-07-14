acdf3826071f194d6e30eaa6f1b3dcde76ca5a77
Abdul Razzaq <arrazzaq7860@gmail.com>
2025-07-15 02:12:07 +0530

    small fix

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -46,7 +46,7 @@ function Navbar() {
               <Link
                 to="/create-member"
                 className={`px-3 py-2 rounded-md text-sm font-medium ${
-                  isActive('/members')
+                  isActive('/create-member')
                     ? 'bg-blue-100 text-blue-700 dark:bg-blue-900 dark:text-blue-200'
                     : 'text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white'
                 }`}
