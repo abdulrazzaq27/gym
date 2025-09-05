55c11024d756adfee40aae7e1db4dfe9dd725a6e
Abdul Razzaq <arrazzaq7860@gmail.com>
2025-09-05 15:21:18 +0530

    Improved dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -90,7 +90,7 @@ function Dashboard() {
 
       {/* Stats Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full mb-8 ">
-        <div className="bg-gray-800 border border-gray-600 rounded-lg p-6">
+        {/* <div className="bg-gray-800 border border-gray-600 rounded-lg p-6">
           <div className="flex items-center justify-between">
             <div>
               <p className="text-gray-400 text-sm font-medium">Total Members</p>
@@ -130,7 +130,7 @@ function Dashboard() {
               </svg>
             </div>
           </div>
-        </div>
+        </div> */}
 
         <Link to="/revenue" className="bg-gray-800 border border-gray-600 rounded-lg p-6 block hover:bg-gray-700 transition-colors">
           <div className="flex items-center justify-between">
@@ -142,6 +142,21 @@ function Dashboard() {
             <div className="bg-yellow-900 p-3 rounded-full">
               <svg className="w-6 h-6 text-yellow-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1" />
+              </svg>
+            </div>
+          </div>
+        </Link>
+
+        <Link to="/attendance" className="bg-gray-800 border border-gray-600 rounded-lg p-6 block hover:bg-gray-700 transition-colors">
+          <div className="flex items-center justify-between">
+            
+            <div>
+              <p className="text-gray-400 text-sm font-medium">Attendance till Date</p>
+              {/* <p className="text-2xl font-bold text-yellow-400">{formatCurrency(dashboardData.totalRevenue)}</p> */}
+            </div>
+            <div className="bg-blue-900 p-3 rounded-full">
+              <svg className="w-6 h-6 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
             </div>
           </div>
