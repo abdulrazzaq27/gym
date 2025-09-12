3663cd030593a5f3a1b4f081b6bd28ae01b7648b
Abdul Razzaq <arrazzaq7860@gmail.com>
2025-09-12 23:30:22 +0530

    Backend Deployed

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose')
-require('dotenv').config({path: './server/.env'});
-
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './server/.env' });
 
 async function connectDB() {
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Connected to DB")
-    }
-    catch(err){
-        console.error('MongoDB connection error:', err);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(
+      '✅ MongoDB connected: ${conn.connection.host}/${conn.connection.name}'
+    );
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1);
+  }
 }
-module.exports = connectDB;
+
+module.exports = connectDB;
