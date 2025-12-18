05a00617e6d0c4aa2bb07ea749bcb4fcde7aa006
Abdul Razzaq <arrazzaq7860@gmail.com>
2025-12-18 15:44:03 +0530

    Production build ready

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: API_BASE_URL,
   // baseURL: "http://192.168.10.110:3000",
 });
 
